54150afda1936ab489bce7fe63ac058923399e53
pdelboca <patriciodelboca@gmail.com>
2017-07-10 21:51:46 -0300

    Refactor para que sea mas facil agregar nuevos candidatos

--- analisisDeLikes.R
+++ analisisDeLikes.R
@@ -3,38 +3,25 @@ library(ggplot2)
 library(readr)
 
 likesLlaryora <- read_csv("./data/martinllaryoraoficial/likes.csv")
-likesBaldassi <- readr::read_csv("./data/hectorbaldassi/likes.csv")
-likesOliviero <- readr::read_csv("./data/liliolivero/likes.csv")
+likesBaldassi <- read_csv("./data/hectorbaldassi/likes.csv")
+likesOliviero <- read_csv("./data/liliolivero/likes.csv")
 
+likesLlaryora$candidato <- "Llaryora"
+likesBaldassi$candidato <- "Baldassi"
+likesOliviero$candidato <- "Oliviero"
 
-x1 <- likesLlaryora %>% 
-  group_by(from_id) %>%
-  count(from_id, sort = TRUE) %>%
-  ungroup() %>%
+datosLikes <- rbind(likesLlaryora, likesBaldassi, likesOliviero)
+
+datosLikes %>%
+  group_by(candidato, from_id) %>%
+  count(candidato, from_id) %>%
+  arrange(candidato, desc(n)) %>%
   mutate(cumsum = cumsum(n),
-         proporcion_likes = cumsum / nrow(likesLlaryora),
-         proporcion_usuarios = row_number() / length(unique(likesLlaryora$from_id)))
+         proporcion_likes = cumsum / sum(n),
+         proporcion_usuarios = row_number() / n()) -> cumLikes
 
-x2 <- likesBaldassi %>% 
-  group_by(from_id) %>%
-  count(from_id, sort = TRUE) %>%
-  ungroup() %>%
-  mutate(cumsum = cumsum(n),
-         proporcion_likes = cumsum / nrow(likesBaldassi),
-         proporcion_usuarios = row_number() / length(unique(likesBaldassi$from_id)))
-
-x3 <- likesOliviero %>% 
-  group_by(from_id) %>%
-  count(from_id, sort = TRUE) %>%
-  ungroup() %>%
-  mutate(cumsum = cumsum(n),
-         proporcion_likes = cumsum / nrow(likesOliviero),
-         proporcion_usuarios = row_number() / length(unique(likesOliviero$from_id)))
-
-ggplot() + 
-  geom_line(data = x1, aes(x=proporcion_usuarios, y=proporcion_likes), color='steelblue', size=1.5) +
-  geom_line(data = x2, aes(x=proporcion_usuarios, y=proporcion_likes), color='green', size=1.5) +
-  geom_line(data = x3, aes(x=proporcion_usuarios, y=proporcion_likes), color='black', size=1.5) +
+ggplot(cumLikes, aes(x=proporcion_usuarios, y=proporcion_likes, color=candidato)) +
+  geom_line(size=1.5) +
   geom_vline(mapping=aes(xintercept=.1), color="red", linetype=2) +
   geom_hline(mapping=aes(yintercept=.6), color="red", linetype=2) +
   labs(y =  "Porcentaje de Likes", 
