ce88a22a25b11f03c97eddc9d6a963fa5df72d6f
pdelboca <patriciodelboca@gmail.com>
2017-07-10 22:12:47 -0300

    Corregido Labels y subttle para que se entienda mejor el grafico.

--- analisisDeLikes.R
+++ analisisDeLikes.R
@@ -25,9 +25,9 @@ ggplot(cumLikes, aes(x=proporcion_usuarios, y=proporcion_likes, color=candidato)
   geom_vline(mapping=aes(xintercept=.1), color="red", linetype=2) +
   geom_hline(mapping=aes(yintercept=.6), color="red", linetype=2) +
   labs(y =  "Porcentaje de Likes", 
-       x =  "Porcentaje de Seguidores",
-       title = "Likes vs Seguidores (para tres Candidatos)",
-       subtitle = "El 10% de los seguidores en Facebook explica aproximadamente el 60% de los likes.") + 
+       x =  "Porcentaje de Seguidores que dieron Like",
+       title = "Likes vs Seguidores",
+       subtitle = "Del total de usuarios que dieron like, el 10% de ellos explica 60% total de Likes.") + 
   scale_y_continuous(labels = scales::percent, 
                      breaks = seq(0,1,.1),
                      minor_breaks = FALSE) +
