38d0daf76d65211fecdfd7d4dc5cd4fce89b6b78
pdelboca <patriciodelboca@gmail.com>
2017-07-12 18:47:00 -0300

    Refactor para que sea mas facil agregar nuevos candidatos

--- palabrasMasUsadas.R
+++ palabrasMasUsadas.R
@@ -11,30 +11,15 @@ postsOliviero <- readr::read_csv("./data/liliolivero//posts.csv")
 
 topN <- 15
 
-topLlaryora <- postsLlaryora %>% 
+bind_rows(postsBaldassi, postsLlaryora, postsOliviero) %>%
   select(from_name, message) %>%
   unnest_tokens(palabra, message) %>%
   filter(!(palabra %in% stopWords)) %>%
   filter(!(is.na(palabra))) %>%
-  count(palabra, sort = TRUE) %>%
-  head(topN)
-
-topBaldassi <- postsBaldassi %>% 
-  select(from_name, message) %>%
-  unnest_tokens(palabra, message) %>%
-  filter(!(palabra %in% stopWords)) %>%
-  filter(!(is.na(palabra))) %>%
-  count(palabra, sort = TRUE) %>%
-  head(topN)
-
-topOliviero <- postsOliviero %>% 
-  select(from_name, message) %>%
-  unnest_tokens(palabra, message) %>%
-  filter(!(palabra %in% stopWords)) %>%
-  count(palabra, sort = TRUE) %>%
-  head(topN)
-
-rankingPalabrasCandidatos <- data.frame(ranking = 1:topN, 
-           Llaryora = topLlaryora$palabra,
-           Baldassi = topBaldassi$palabra,
-           Oliviero = topOliviero$palabra)
+  group_by(from_name) %>%
+  count(palabra) %>%
+  arrange(from_name, desc(n)) %>%
+  slice(1:topN) %>%
+  mutate(ranking = row_number()) %>%
+  select(from_name, palabra, ranking) %>%
+  tidyr::spread(from_name, palabra) -> rankingPalabrasCandidatos
